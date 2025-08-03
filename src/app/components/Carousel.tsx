ca3f199f7070fe405131414975ed4b82ef2b65f9
LucasSousa <lucassousasertania@gmail.com>
2025-08-03 18:28:27 -0300

    feat(carousel): add Carousel component with image display

---
+++ src/app/components/Carousel.tsx
@@ -0,0 +1,11 @@
+import { createClient } from "../utils/supabase/server"
+
+export default async function Carousel() {
+
+
+    return (
+        <div className="container justify-center items-center flex mx-auto mt-4">
+            <img src="/banner.jpg" alt="" />
+        </div>
+    )
+}
