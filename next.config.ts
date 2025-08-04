47dba5722ea4e5da88e0943a307471cc537fae9d
LucasSousa <lucassousasertania@gmail.com>
2025-08-04 15:52:38 -0300

    feat(config): add image remote patterns for optimized loading

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://m.media-amazon.com/**"),
+      new URL("https://ae-pic-a1.aliexpress-media.com/**"),
+      new URL("https://http2.mlstatic.com/**"),
+      new URL("https://vasorama.com.br/**"),
+      new URL("https://static.estantevirtual.com.br/**"),
+      new URL("https://basico.com/**"),
+      new URL("https://product-data.raiadrogasil.io/**"),
+      new URL("https://images.tcdn.com.br/**"),
+      new URL("https://img.ltwebstatic.com/**"),
+      new URL("https://zaffari.vtexassets.com/**"),
+      // new URL("/**"),
+    ]
+  }
 };
 
 export default nextConfig;
